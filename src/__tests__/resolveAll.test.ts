26ef777f61c9f24a21b94214a66fd992534a92b4
Elie rotenberg <elie@rotenberg.io>
2020-12-16 10:11:22 +0100

    1.5.7: simplify typing of resolveAll
    * use TS 4.1 variadic tuple type instead of death by a thousand overload

--- src/__tests__/resolveAll.test.ts
+++ src/__tests__/resolveAll.test.ts
@@ -2,6 +2,7 @@ import { resolveAll } from "../resolveAll";
 
 test("resolveAll", async () => {
   const t = [0, 3, null];
+  // typeof p1 = [0, 3, null]
   const p1 = await resolveAll([
     Promise.resolve(0),
     Promise.resolve(3),
@@ -9,6 +10,7 @@ test("resolveAll", async () => {
   ]);
   expect(p1).toEqual(t);
 
+  // typeof p2 = (number | null)[]
   const p2 = await resolveAll([0, 3, null].map((v) => Promise.resolve(v)));
   expect(p2).toEqual(t);
 
--- src/resolveAll.ts
+++ src/resolveAll.ts
@@ -1,82 +1,22 @@
-type ResolveAll1 = <T1>(values: [Promise<T1>]) => Promise<[T1]>;
-type ResolveAll2 = <T1, T2>(
-  values: [Promise<T1>, Promise<T2>],
-) => Promise<[T1, T2]>;
-type ResolveAll3 = <T1, T2, T3>(
-  values: [Promise<T1>, Promise<T2>, Promise<T3>],
-) => Promise<[T1, T2, T3]>;
-type ResolveAll4 = <T1, T2, T3, T4>(
-  values: [Promise<T1>, Promise<T2>, Promise<T3>, Promise<T4>],
-) => Promise<[T1, T2, T3, T4]>;
-type ResolveAll5 = <T1, T2, T3, T4, T5>(
-  values: [Promise<T1>, Promise<T2>, Promise<T3>, Promise<T4>, Promise<T5>],
-) => Promise<[T1, T2, T3, T4, T5]>;
-type ResolveAll6 = <T1, T2, T3, T4, T5, T6>(
-  values: [
-    Promise<T1>,
-    Promise<T2>,
-    Promise<T3>,
-    Promise<T4>,
-    Promise<T5>,
-    Promise<T6>,
-  ],
-) => Promise<[T1, T2, T3, T4, T5, T6]>;
-type ResolveAll7 = <T1, T2, T3, T4, T5, T6, T7>(
-  values: [
-    Promise<T1>,
-    Promise<T2>,
-    Promise<T3>,
-    Promise<T4>,
-    Promise<T5>,
-    Promise<T6>,
-    Promise<T7>,
-  ],
-) => Promise<[T1, T2, T3, T4, T5, T6, T7]>;
-type ResolveAll8 = <T1, T2, T3, T4, T5, T6, T7, T8>(
-  values: [
-    Promise<T1>,
-    Promise<T2>,
-    Promise<T3>,
-    Promise<T4>,
-    Promise<T5>,
-    Promise<T6>,
-    Promise<T7>,
-    Promise<T8>,
-  ],
-) => Promise<[T1, T2, T3, T4, T5, T6, T7, T8]>;
-type ResolveAll9 = <T1, T2, T3, T4, T5, T6, T7, T8, T9>(
-  values: [
-    Promise<T1>,
-    Promise<T2>,
-    Promise<T3>,
-    Promise<T4>,
-    Promise<T5>,
-    Promise<T6>,
-    Promise<T7>,
-    Promise<T8>,
-    Promise<T9>,
-  ],
-) => Promise<[T1, T2, T3, T4, T5, T6, T7, T8, T9]>;
-type ResolveAllIter = <T>(values: Promise<T>[]) => Promise<T[]>;
-type ResolveAll = ResolveAll1 &
-  ResolveAll2 &
-  ResolveAll3 &
-  ResolveAll4 &
-  ResolveAll5 &
-  ResolveAll6 &
-  ResolveAll7 &
-  ResolveAll8 &
-  ResolveAll9 &
-  ResolveAllIter;
+type ResolveAll = <T extends readonly unknown[] | readonly [unknown]>(
+  values: T,
+) => Promise<
+  {
+    -readonly [P in keyof T]: T[P] extends Promise<infer U> ? U : T[P];
+  }
+>;
 
-export const resolveAll = (async <T extends Promise<unknown>>(
-  values: T[],
-): Promise<T[]> => {
+type AllSettledResult<T> = Readonly<
+  | { readonly status: "rejected"; readonly reason: unknown }
+  | { readonly status: "fulfilled"; readonly value: T }
+>;
+
+export const resolveAll: ResolveAll = (async (values) => {
   const results = await Promise.allSettled(values);
-  return results.map((result) => {
+  return ((results as unknown) as AllSettledResult<never>[]).map((result) => {
     if (result.status === "rejected") {
       throw result.reason;
     }
     return result.value;
-  }) as T[];
+  });
 }) as ResolveAll;
