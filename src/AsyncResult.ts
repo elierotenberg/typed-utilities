85ac6c888c38759deba44bf3f65940e5fab99e8c
Elie Rotenberg <elie@rotenberg.io>
2021-04-01 13:59:48 +0200

    1.8.3: implement AsyncResult.join
    * Allow combining multiple AsyncResult into an aggregate
    * Refine generic typing for Async.of.* using never

--- src/AsyncResult.ts
+++ src/AsyncResult.ts
@@ -1,3 +1,7 @@
+import AggregateError from "aggregate-error";
+
+import { AsyncResult } from ".";
+
 enum AsyncResultTag {
   Pending = "pending",
   Resolved = "resolved",
@@ -73,12 +77,16 @@ export const assert = {
 };
 
 export const of = {
-  pending: (): Pending => [AsyncResultTag.Pending],
-  rejected: <Error = unknown>(error: Error): Rejected<Error> => [
+  pending: (): Pending & AsyncResult<never, never> => [AsyncResultTag.Pending],
+  rejected: <Error = unknown>(
+    error: Error,
+  ): Rejected<Error> & AsyncResult<never, Error> => [
     AsyncResultTag.Rejected,
     error,
   ],
-  resolved: <Value = unknown>(value: Value): Resolved<Value> => [
+  resolved: <Value = unknown>(
+    value: Value,
+  ): Resolved<Value> & AsyncResult<Value, never> => [
     AsyncResultTag.Resolved,
     value,
   ],
@@ -116,3 +124,103 @@ export const match = <
     : isRejected(result)
     ? match.rejected(to.rejectedError(result))
     : match.resolved(to.resolvedValue(result));
+
+type Join = {
+  (values: readonly []): AsyncResult<[]>;
+  <V1, E1 extends Error>(values: readonly [AsyncResult<V1, E1>]): AsyncResult<
+    [V1],
+    AggregateError<E1>
+  >;
+  <V1, E1 extends Error, V2, E2 extends Error>(
+    values: readonly [AsyncResult<V1, E1>, AsyncResult<V2, E2>],
+  ): AsyncResult<[V1, V2], AggregateError<E1 | E2>>;
+  <V1, E1 extends Error, V2, E2 extends Error, V3, E3 extends Error>(
+    values: readonly [
+      AsyncResult<V1, E1>,
+      AsyncResult<V2, E2>,
+      AsyncResult<V3, E3>,
+    ],
+  ): AsyncResult<[V1, V2, V3], AggregateError<E1 | E2 | E3>>;
+  <
+    V1,
+    E1 extends Error,
+    V2,
+    E2 extends Error,
+    V3,
+    E3 extends Error,
+    V4,
+    E4 extends Error
+  >(
+    values: readonly [
+      AsyncResult<V1, E1>,
+      AsyncResult<V2, E2>,
+      AsyncResult<V3, E3>,
+      AsyncResult<V4, E4>,
+    ],
+  ): AsyncResult<[V1, V2, V3, V4], AggregateError<E1 | E2 | E3 | E4>>;
+  <
+    V1,
+    E1 extends Error,
+    V2,
+    E2 extends Error,
+    V3,
+    E3 extends Error,
+    V4,
+    E4 extends Error,
+    V5,
+    E5 extends Error
+  >(
+    values: readonly [
+      AsyncResult<V1, E1>,
+      AsyncResult<V2, E2>,
+      AsyncResult<V3, E3>,
+      AsyncResult<V4, E4>,
+      AsyncResult<V5, E5>,
+    ],
+  ): AsyncResult<[V1, V2, V3, V4, V5], AggregateError<E1 | E2 | E3 | E4 | E5>>;
+  <
+    V1,
+    E1 extends Error,
+    V2,
+    E2 extends Error,
+    V3,
+    E3 extends Error,
+    V4,
+    E4 extends Error,
+    V5,
+    E5 extends Error,
+    V6,
+    E6 extends Error
+  >(
+    values: readonly [
+      AsyncResult<V1, E1>,
+      AsyncResult<V2, E2>,
+      AsyncResult<V3, E3>,
+      AsyncResult<V4, E4>,
+      AsyncResult<V5, E5>,
+      AsyncResult<V6, E6>,
+    ],
+  ): AsyncResult<
+    [V1, V2, V3, V4, V5, V6],
+    AggregateError<E1 | E2 | E3 | E4 | E5 | E6>
+  >;
+  <T, E extends Error>(values: readonly AsyncResult<T>[]): AsyncResult<
+    T[],
+    AggregateError<E>
+  >;
+};
+
+export const join = ((results: readonly AsyncResult<unknown>[]) => {
+  const pending = results.filter(AsyncResult.is.pending);
+  const rejected = results.filter(AsyncResult.is.rejected);
+  const resolved = results.filter(AsyncResult.is.resolved);
+  if (pending.length > 0) {
+    return AsyncResult.of.pending();
+  }
+  if (rejected.length > 0) {
+    return AsyncResult.of.rejected(
+      new AggregateError(rejected.map(AsyncResult.to.rejectedError) as Error[]),
+    );
+  }
+  return AsyncResult.of.resolved(resolved.map(AsyncResult.to.resolvedValue));
+}) as Join;
--- src/__tests__/AsyncResult.test.ts
+++ src/__tests__/AsyncResult.test.ts
@@ -1,41 +1,107 @@
-import { AsyncResult } from "..";
+import AggregateError from "aggregate-error";
 
-test("AsyncResult", async () => {
-  const testError = new Error("test error");
-  const pending = AsyncResult.of.pending();
-  const rejected = AsyncResult.of.rejected(testError);
-  const resolved = AsyncResult.of.resolved(null);
-  expect(AsyncResult.is.pending(pending)).toEqual(true);
-  expect(AsyncResult.is.rejected(pending)).toEqual(false);
-  expect(AsyncResult.is.resolved(pending)).toEqual(false);
-  expect(() => AsyncResult.assert.pending(pending)).not.toThrow();
-  expect(() => AsyncResult.assert.rejected(pending)).toThrow();
-  expect(() => AsyncResult.assert.resolved(pending)).toThrow();
+import { AsyncResult, range } from "..";
 
-  expect(AsyncResult.is.pending(rejected)).toEqual(false);
-  expect(AsyncResult.is.rejected(rejected)).toEqual(true);
-  expect(AsyncResult.is.resolved(rejected)).toEqual(false);
-  expect(() => AsyncResult.assert.pending(rejected)).toThrow();
-  expect(() => AsyncResult.assert.rejected(rejected)).not.toThrow();
-  expect(() => AsyncResult.assert.resolved(rejected)).toThrow();
+describe("AsyncResult", () => {
+  test("basic", async () => {
+    const testError = new Error("test error");
+    const pending = AsyncResult.of.pending();
+    const rejected = AsyncResult.of.rejected(testError);
+    const resolved = AsyncResult.of.resolved(null);
+    expect(AsyncResult.is.pending(pending)).toEqual(true);
+    expect(AsyncResult.is.rejected(pending)).toEqual(false);
+    expect(AsyncResult.is.resolved(pending)).toEqual(false);
+    expect(() => AsyncResult.assert.pending(pending)).not.toThrow();
+    expect(() => AsyncResult.assert.rejected(pending)).toThrow();
+    expect(() => AsyncResult.assert.resolved(pending)).toThrow();
 
-  expect(AsyncResult.is.pending(resolved)).toEqual(false);
-  expect(AsyncResult.is.rejected(resolved)).toEqual(false);
-  expect(AsyncResult.is.resolved(resolved)).toEqual(true);
-  expect(() => AsyncResult.assert.pending(resolved)).toThrow();
-  expect(() => AsyncResult.assert.rejected(resolved)).toThrow();
-  expect(() => AsyncResult.assert.resolved(resolved)).not.toThrow();
+    expect(AsyncResult.is.pending(rejected)).toEqual(false);
+    expect(AsyncResult.is.rejected(rejected)).toEqual(true);
+    expect(AsyncResult.is.resolved(rejected)).toEqual(false);
+    expect(() => AsyncResult.assert.pending(rejected)).toThrow();
+    expect(() => AsyncResult.assert.rejected(rejected)).not.toThrow();
+    expect(() => AsyncResult.assert.resolved(rejected)).toThrow();
 
-  expect(AsyncResult.to.rejectedError(rejected)).toEqual(testError);
-  expect(AsyncResult.to.resolvedValue(resolved)).toEqual(null);
+    expect(AsyncResult.is.pending(resolved)).toEqual(false);
+    expect(AsyncResult.is.rejected(resolved)).toEqual(false);
+    expect(AsyncResult.is.resolved(resolved)).toEqual(true);
+    expect(() => AsyncResult.assert.pending(resolved)).toThrow();
+    expect(() => AsyncResult.assert.rejected(resolved)).toThrow();
+    expect(() => AsyncResult.assert.resolved(resolved)).not.toThrow();
 
-  const match = {
-    pending: () => "pending",
-    rejected: (error: Error) => `error: ${error.message}`,
-    resolved: (value: null) => `value: ${value}`,
-  };
+    expect(AsyncResult.to.rejectedError(rejected)).toEqual(testError);
+    expect(AsyncResult.to.resolvedValue(resolved)).toEqual(null);
 
-  expect(AsyncResult.match(pending, match)).toEqual("pending");
-  expect(AsyncResult.match(rejected, match)).toEqual("error: test error");
-  expect(AsyncResult.match(resolved, match)).toEqual("value: null");
+    const match = {
+      pending: () => "pending",
+      rejected: (error: Error) => `error: ${error.message}`,
+      resolved: (value: null) => `value: ${value}`,
+    };
+
+    expect(AsyncResult.match(pending, match)).toEqual("pending");
+    expect(AsyncResult.match(rejected, match)).toEqual("error: test error");
+    expect(AsyncResult.match(resolved, match)).toEqual("value: null");
+  });
+
+  test("join", () => {
+    const error1 = new Error("error1");
+    const error2 = new TypeError("error2");
+
+    expect(AsyncResult.join([])).toEqual(AsyncResult.of.resolved([]));
+    expect(AsyncResult.join([AsyncResult.of.pending()])).toEqual(
+      AsyncResult.of.pending(),
+    );
+    expect(AsyncResult.join([AsyncResult.of.rejected(error1)])).toEqual(
+      AsyncResult.of.rejected(new AggregateError([error1])),
+    );
+    expect(AsyncResult.join([AsyncResult.of.resolved(null)])).toEqual(
+      AsyncResult.of.resolved([null]),
+    );
+
+    const t1 = [
+      AsyncResult.of.pending(),
+      AsyncResult.of.rejected(error1),
+      AsyncResult.of.resolved(null),
+    ] as const;
+    const j1: AsyncResult.AsyncResult<
+      [never, never, null],
+      Error
+    > = AsyncResult.join(t1);
+    expect(j1).toEqual(AsyncResult.of.pending());
+
+    const t2 = [
+      AsyncResult.of.rejected(error1),
+      AsyncResult.of.resolved(null),
+    ] as const;
+    const j2: AsyncResult.AsyncResult<[never, null], Error> = AsyncResult.join(
+      t2,
+    );
+    expect(j2).toEqual(AsyncResult.of.rejected(new AggregateError([error1])));
+
+    const t3 = [
+      AsyncResult.of.resolved(null),
+      AsyncResult.of.rejected(error1),
+      AsyncResult.of.rejected(error2),
+    ] as const;
+    const j3: AsyncResult.AsyncResult<
+      [null, never, never],
+      Error | TypeError
+    > = AsyncResult.join(t3);
+    expect(j3).toEqual(
+      AsyncResult.of.rejected(new AggregateError([error1, error2])),
+    );
+
+    const t4 = [
+      AsyncResult.of.resolved(null),
+      AsyncResult.of.resolved(0 as const),
+      AsyncResult.of.resolved(["a"] as const),
+    ] as const;
+    const j4: AsyncResult.AsyncResult<
+      [null, 0, readonly ["a"]]
+    > = AsyncResult.join(t4);
+    expect(j4).toEqual(AsyncResult.of.resolved([null, 0, ["a"]]));
+
+    const t5 = range(10).map((k) => AsyncResult.of.resolved(k));
+    expect(AsyncResult.join(t5)).toEqual(AsyncResult.of.resolved(range(10)));
+  });
 });
